68d0adb88b108f087f1b08833c78732daa9299fb
Hemant <kumarhemant99108@gmail.com>
2026-05-05 16:00:59 +0530

    nothng to do 0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+   
     <BrowserRouter>
       <App />
       <Toaster
@@ -17,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         }}
       />
     </BrowserRouter>
-  </React.StrictMode>
+   
 )
